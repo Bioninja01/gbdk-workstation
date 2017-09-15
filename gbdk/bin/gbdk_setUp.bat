ab288c0a389e6a59ea248efecbbd62197672080f
James Vinson <vinsonjames02@gmail.com>
2017-09-15 17:53:54 -0500

    init
    init

---
+++ gbdk/bin/gbdk_setUp.bat
@@ -0,0 +1,2 @@
+cd C:\gbdk-2.95-3-win32\gbdk\examples\JamesSandBox
+C:\gbdk-2.95-3-win32\gbdk\doc\libc\html\gb.h.html
---
+++ gbdk/examples/JamesSandBox/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <gb/gb.h>
+#include "tileTest.h"
+
+int main(){
+    UBYTE x = 1;
+    printf("%d helloWorld",x);
+    return 0;
+}
